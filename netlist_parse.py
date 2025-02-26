00264423a2bebb82f1b5959c74e43f7f86522169
--local <aidancisneros092@tamu.edu>
2025-02-26 13:12:54 -0600

    Added netlist file path to class and floated values. Cleaned up comments.

--- netlist_parse.py
+++ netlist_parse.py
@@ -1,3 +1,4 @@
+# Class Declaration
 class Component:
     def __init__(self, name="", type="", value=0, variable=False, modified=False):
         self.name = name
@@ -7,15 +8,16 @@ class Component:
         self.modified = modified
 
 class Netlist:
-    def __init__(self, file_name):
-        self.components = self.parse_file(file_name)
+    def __init__(self, file_path):
+        self.components = self.parse_file(file_path)
+        self.file_path = file_path
 
-    def parse_file(self, file_name) -> list:
+    def parse_file(self, file_path) -> list:
+    # Current Behavior: Parses file for RLC values to place into netlist's list. Skips Title Line, Commands, and non RLC components
         components = []
         try:
-            with open(file_name,"r") as file:
-            #------Parsing Logic------#
-            #Current Behavior: Skip Title Line, Commands, and non RLC components
+            with open(file_path,"r") as file:
+            #Parsing Logic
                 file.readline()
                 for line in file:
                     values=line.strip().split(" ")
@@ -23,70 +25,58 @@ class Netlist:
                         continue
                     if(values[0][0] != "R" and values[0][0] != "L" and values[0][0] != "C"):
                         continue
-#####################TODO: Need to deal with component value conversions#####################
-
-                    newComponent = Component(values[0],values[0][0],values[3])
+##################### TODO: Need to deal with component value conversions possibly. Just floating right now, but doesn't help for 1K, 31u, etc.#####################
+                    newComponent = Component(values[0],values[0][0],float(values[3]))
                     components.append(newComponent)
-                    print(values)
-                    #print(line.strip())
-                    #print("Hello")
-
         except FileNotFoundError:
-            print(f"Error: The file '{file_name}' was not found.")
+            print(f"Error: The file '{file_path}' was not found.")
         except Exception as e:
             print(f"An error occurred: {e}")
         return components
     
-    def class_to_file(self, file_name):
+    def class_to_file(self, file_path):
+    # Current Behavior: Reads in specified file and updates lines matching lines in the netlist class that have been marked as modified with the new value.
         try:
-            #Get Current file netlist and modified components in class
-            with open(file_name,"r") as file:
+            # Get Current file netlist and modified components in class
+            with open(file_path,"r") as file:
                 data = file.readlines()
             modifiedComponents = []
             for component in self.components:
                 if component.modified == True:
                     modifiedComponents.append(component)
                     component.modified = False
-            #print("---------------PRE-CHANGE DATA-----------------")
-            #print(data)
-            #print("---------------MODIFIED COMPONENTS LIST-----------------")
-            #print(modifiedComponents)
-            #Generate updated netlist
+            # Generate updated netlist
             updatedData=[]
             for line in data:
                 lineData = line.strip().split(" ")
                 for component in modifiedComponents:
                     if lineData[0] == component.name:
-                        #print("MATCH FOUND!")
-                        #print(line)
-                        #print(f"LineData[3] = {lineData[3]}")
                         lineData[3] = component.value
-                        #print(f"LineData[3] = {lineData[3]}")
-                        line = f"{lineData[0]} {lineData[1]} {lineData[2]} {lineData[3]}\n"
-                        #print(line)
+                        line = f"{lineData[0]} {lineData[1]} {lineData[2]} {float(lineData[3])}\n"
                         modifiedComponents.remove(component)
                         break
                 updatedData.append(line)
-            #print("---------------POST-CHANGE DATA-----------------")
-            #print(updatedData)
-            #print("---------------REMAINING MODIFIED COMPONENTS LIST-----------------")
-            #print(modifiedComponents)
-            with open(file_name,"w") as file:
+            with open(file_path,"w") as file:
                 file.writelines(updatedData)
         except FileNotFoundError:
-            print(f"Error: The file '{file_name}' was not found.")
+            print(f"Error: The file '{file_path}' was not found.")
         except Exception as e:
             print(f"An error occurred: {e}")
 
-
-myNetlist = Netlist("buck.txt")
-
+# Test Statements
+# myNetlist = Netlist("buck.txt")
 # print("Values pre class to file:")
 # print(myNetlist.components[2].name)
 # print(myNetlist.components[2].type)
 # print(myNetlist.components[2].value)
 # print(myNetlist.components[2].variable)
 # print(myNetlist.components[2].modified)
+
+# print(type(myNetlist.components[2].name))
+# print(type(myNetlist.components[2].type))
+# print(type(myNetlist.components[2].value))
+# print(type(myNetlist.components[2].variable))
+# print(type(myNetlist.components[2].modified))
 
 # print(myNetlist.components[3].name)
 # print(myNetlist.components[3].type)
@@ -96,9 +86,9 @@ myNetlist = Netlist("buck.txt")
 
 
 # myNetlist.components[2].modified = True
-# myNetlist.components[2].value = "2021"
+# myNetlist.components[2].value = 2021
 # myNetlist.components[3].modified = True
-# myNetlist.components[3].value = "2025"
+# myNetlist.components[3].value = 2025
 # myNetlist.class_to_file("buck.txt")
 
 # print("Values post class to file:")
@@ -107,6 +97,12 @@ myNetlist = Netlist("buck.txt")
 # print(myNetlist.components[2].value)
 # print(myNetlist.components[2].variable)
 # print(myNetlist.components[2].modified)
+
+# print(type(myNetlist.components[2].name))
+# print(type(myNetlist.components[2].type))
+# print(type(myNetlist.components[2].value))
+# print(type(myNetlist.components[2].variable))
+# print(type(myNetlist.components[2].modified))
 
 # print(myNetlist.components[3].name)
 # print(myNetlist.components[3].type)
