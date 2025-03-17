a398bfbd6a01471b42c6a7bbdd3188e9507e8cb6
brand727 <brandonocisneros@gmail.com>
2025-03-17 13:15:47 -0500

    constraints fundamentals

--- curvefit_optimization.py
+++ curvefit_optimization.py
@@ -6,8 +6,42 @@ from scipy.interpolate import interp1d
 from xyce_parsing_function import parse_xyce_prn_output
 from netlist_parse import Netlist
 
+"""
+Two constraint types:
+1. Node value constraints
+    These will be solved by checking output after a Xyce run, then skewing the residual output so it won't select it if the voltage constraint is breached
+    This will probably mess up next part value selection if if not done well
+2. Part value constraints
+    These are simpler to do and can be done using bounds arg of least_squares
+    Equation type ones (i.e. R1 + R2 < 4000) are trickier perhaps
+"""
+# Part Value = Constant
+#   TODO (but not really because Not really a case because it would just not be a variable)
+# Node Value = Constant
+#   TODO
+# Part Value <, > Constant
+#   TEST by using bounds arg
+# Node Value <, > Constant
+#   TEST by throwing out/skewing Xyce run output
+# Part Value = Equation
+#   TODO 
+# Node Value = Equation
+#   TODO
+# Part Value <, > Equation
+#   TODO
+# Node Value <, > Equation
+#   TODO
 
-def curvefit_optimize(target_value: str, target_curve_rows: list, netlist: Netlist, writable_netlist_path: str) -> None:
+# TODO: Update Component class with min_value, max_value when constratints are set
+"""
+node_constraints structure
+
+node_constraints = {
+    'V(2)': (None, 5.0),  # Example: V(2) must be <= 5V
+    'V(3)': (1.0, None)   # Example: V(3) must be >= 1V
+}
+"""
+def curvefit_optimize(target_value: str, target_curve_rows: list, netlist: Netlist, writable_netlist_path: str, node_constraints: dict) -> None:
     # Assumes input_curve[0] is X, input_curve[1] is Y/target_value
     x_ideal = np.array([x[0] for x in target_curve_rows])
     y_ideal = np.array([x[1] for x in target_curve_rows])
@@ -19,6 +53,11 @@ def curvefit_optimize(target_value: str, target_curve_rows: list, netlist: Netli
     # Parse netlist to figure out which parts are subject to change
     changing_components = [x for x in netlist.components if x.variable]
     changing_components_values = [x.value for x in changing_components]
+
+    lower_bounds = np.array([x.min_value if hasattr(x, "min_value") else -np.inf for x in changing_components])
+    upper_bounds = np.array([x.max_value if hasattr(x, "max_value") else np.inf for x in changing_components])
+
+
     # print(changing_components_values)
     run_state = {
         "first_run": True,
@@ -58,9 +97,19 @@ def curvefit_optimize(target_value: str, target_curve_rows: list, netlist: Netli
         # print("Y_ARRAY: ", Y_ARRAY_FROM_XYCE)
         # print("SHAPES: ", X_ARRAY_FROM_XYCE.shape, ideal_interpolation(X_ARRAY_FROM_XYCE).shape, Y_ARRAY_FROM_XYCE.shape, xyce_interpolation(run_state["master_x_points"]).shape)
         # TODO: Proper residual? (subrtarct, rms, etc.)
+        # Check node constraint violations
+        # TODO: Is an inf penalty appropriate?
+        for node_name, (node_lower, node_upper) in node_constraints.items():
+            node_index = xyce_parse[0].index(node_name)
+            node_values = np.array([float(x[node_index]) for x in xyce_parse[1]])
+            if (node_lower is not None and np.any(node_values < node_lower)) or (node_upper is not None and np.any(node_values > node_upper)):
+                return np.full_like(run_state["master_x_points"], np.inf)  # Large penalty
+
+
         return ideal_interpolation(run_state["master_x_points"]) - xyce_interpolation(run_state["master_x_points"])
     
-    result = least_squares(residuals, changing_components_values, method='lm', args=(changing_components,))
+    # TODO: trf vs dogbox vs lm
+    result = least_squares(residuals, changing_components_values, method='lm', bounds=(lower_bounds, upper_bounds), args=(changing_components,))
     # print(result.x)
     for i in range(len(changing_components)):
         changing_components[i].value = result.x[i]
