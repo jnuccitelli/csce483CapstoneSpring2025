c421f9711fa996581daca65eb8001baf281c295b
Katelyn Nguyen <heyitsk@tamu.edu>
2025-03-19 22:58:46 -0500

    LINE and HEAVISIDE frames seem to work. don't know why UPLOAD is always showing :(

--- frontend/optimization_settings/curve_fit_settings.py
+++ frontend/optimization_settings/curve_fit_settings.py
@@ -8,6 +8,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+from enum import Enum
+
+class input_type(Enum):
+    LINE = 1
+    HEAVISIDE = 2
+    UPLOAD = 3
 
 class CurveFitSettings(tk.Frame):
     def __init__(self, parent: tk.Frame, parameters: List[str], nodes):
@@ -17,47 +23,70 @@ class CurveFitSettings(tk.Frame):
         self.x_parameter_expression_var = tk.StringVar()
         self.y_parameter_expression_var = tk.StringVar()
 
-        # --- Curve Fit File Picker ---
-        curve_fit_button = ttk.Button(
-            self, text="Select Curve File", command=self.select_curve_file
-        )
-        curve_fit_button.pack()
+        
 
-        self.curve_file_path_var = tk.StringVar(value="")
-        curve_file_label = ttk.Label(self, textvariable=self.curve_file_path_var)
-        curve_file_label.pack()
+
+
+        # --- combobox for: line input vs heavyside vs custom csv
+        self.select_input_type_frame = ttk.Frame(self)
+        self.select_input_type_frame.pack(side=tk.TOP, fill=tk.X)
+        ttk.Label(self.select_input_type_frame, text="Input Function Type: ").pack(side=tk.LEFT)
+        answer = tk.StringVar()
+        self.input_type_options = ttk.Combobox(self.select_input_type_frame, textvariable=answer)
+        self.input_type_options['values'] = ('Line',
+                                        'Heaviside',
+                                        'Upload')
+        self.input_type_options.pack(side=tk.LEFT)
+        self.input_type_options.current()
+        self.input_type_options.bind("<<ComboboxSelected>>", self.show_frame)
+
+        self.frames = {}
+        self.frames['Line'] = self.create_line_frame()
+        self.frames['Heaviside'] = self.create_heaviside_frame()
+        self.frames['Upload'] = self.create_upload_frame()
+
+        for frame in self.frames.values():
+            frame.pack_forget()
+
+        # self.input_type_frames = {}
+        # for option in ['Line', 'Heaviside', 'Upload']:
+        #     frame = tk.Frame(self.select_input_type_frame)
+        #     label = tk.Label(frame, text=f"Showing: {option}")
+        #     label.pack(side=tk.LEFT)
+        #     self.input_type_frames[option] = frame
 
 
 
         # --- function input here
-        
-        self.function_input_frame = ttk.Frame(self)
-        self.function_input_frame.pack(fill=tk.X, pady=5)
-        ttk.Label(self.function_input_frame, text="y =").pack(side=tk.LEFT)
-        self.y_equals_function_input = ttk.Entry(self.function_input_frame, width=5)
-        self.y_equals_function_input.pack(side=tk.LEFT)
-        ttk.Label(self.function_input_frame, text="from x =").pack(side=tk.LEFT)
-        self.x_start = tk.Entry(self.function_input_frame, width=5)
-        self.x_start.pack(side=tk.LEFT)
-        ttk.Label(self.function_input_frame, text="to x =").pack(side=tk.LEFT)
-        self.x_end = tk.Entry(self.function_input_frame, width=5)
-        self.x_end.pack(side=tk.LEFT)
-        ttk.Button(self.function_input_frame, text="Add Function", command=self.add_function).pack(side=tk.LEFT, padx=10)
-        
-        self.see_inputted_functions = ttk.Frame(self)
-        self.see_inputted_functions.pack(fill=tk.Y, pady=5)
 
-        ttk.Button(self.function_input_frame, text="Plot", command=self.generate_data).pack(side=tk.LEFT, padx=10)
-
-        self.custom_functions = []
-        self.generated_csv = None
-
-        # --- plotting from input here
-        self.figure, self.ax = plt.subplots()
+        # self.function_input_frame = ttk.Frame(self)
+        # self.function_input_frame.pack(fill=tk.X, pady=5)
+        # ttk.Label(self.function_input_frame, text="y =").pack(side=tk.LEFT)
+        # self.y_equals_function_input = ttk.Entry(self.function_input_frame, width=5)
+        # self.y_equals_function_input.pack(side=tk.LEFT)
+        # ttk.Label(self.function_input_frame, text="from x =").pack(side=tk.LEFT)
+        # self.x_start = tk.Entry(self.function_input_frame, width=5)
+        # self.x_start.pack(side=tk.LEFT)
+        # ttk.Label(self.function_input_frame, text="to x =").pack(side=tk.LEFT)
+        # self.x_end = tk.Entry(self.function_input_frame, width=5)
+        # self.x_end.pack(side=tk.LEFT)
         
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self.see_inputted_functions)
-        # self.canvas.draw()
-        self.canvas.get_tk_widget().pack()
+
+        # self.see_inputted_functions = ttk.Frame(self, width=100, height=100)
+        # self.see_inputted_functions.pack(pady=5, side=tk.TOP, expand=False)
+
+        # ttk.Button(self.function_input_frame, text="Add Function", command=self.add_function).pack(side=tk.LEFT, padx=10)
+        # ttk.Button(self.function_input_frame, text="Plot", command=self.generate_data).pack(side=tk.LEFT, padx=10)
+
+        # self.custom_functions = []
+        # self.generated_csv = None
+
+        # # --- plotting from input here
+        # self.figure, self.ax = plt.subplots()
+        
+        # self.canvas = FigureCanvasTkAgg(self.figure, master=self.see_inputted_functions)
+        # # self.canvas.draw()
+        # self.canvas.get_tk_widget().pack(expand=True)
 
 
 
@@ -108,11 +137,90 @@ class CurveFitSettings(tk.Frame):
         )
         y_expression_button.pack(side=tk.LEFT)
         y_param_frame.pack(side=tk.LEFT, padx=5)
+    
+    # def show_frame(self, frame_name):
+    #     for frame in self.input_type_frames.values():
+    #         frame.pack_forget()
+    #     self.input_type_frames[frame_name].pack(pady=20)
+    
+    # def on_combobox_select(self, event):
+    #     selected_option = self.input_type_options.get()
+    #     self.show_frame(selected_option)
 
-    def add_function(self):
-        self.custom_functions.append((self.y_equals_function_input.get(), self.x_start.get(), self.x_end.get()))
-        string_func = "y = " + self.y_equals_function_input.get() + " from x = [" + self.x_start.get() + " to " + self.x_end.get() + "]\n"
-        ttk.Label(self.see_inputted_functions, text=string_func).pack(side=tk.LEFT, pady=10)
+    def create_line_frame(self):
+        line_frame = tk.Frame(self.select_input_type_frame)
+        line_frame.pack(side=tk.TOP, fill=tk.X)
+        tk.Label(line_frame, text="Slope = ").pack(side=tk.LEFT) 
+        line_slope = tk.Entry(line_frame, width=5)
+        line_slope.pack(side=tk.LEFT)# have to separate the pack() into a new line bc it makes the type NONE
+        tk.Label(line_frame, text="From x = ").pack(side=tk.LEFT)
+        line_start_x = tk.Entry(line_frame, width=5)
+        line_start_x.pack(side=tk.LEFT)
+        tk.Label(line_frame, text="to x = ").pack(side=tk.LEFT)
+        line_end_x = tk.Entry(line_frame, width=5)
+        line_end_x.pack(side=tk.LEFT)
+        line_button = tk.Button(line_frame, text="Add Line", command=lambda:
+                   self.add_function(input_type.LINE, line_slope, line_start_x, line_end_x))
+        line_button.pack(side=tk.LEFT, padx=10)
+        self.custom_functions = []
+
+        self.see_inputted_functions = tk.Frame(self.select_input_type_frame).pack(side=tk.LEFT)
+
+        return line_frame
+
+    def create_heaviside_frame(self):
+        heaviside_frame = tk.Frame(self.select_input_type_frame)
+        heaviside_frame.pack(side=tk.TOP, fill=tk.X)
+        tk.Label(heaviside_frame, text="Amplitude = ").pack(side=tk.LEFT) 
+        heaviside_amplitude = tk.Entry(heaviside_frame, width=5)
+        heaviside_amplitude.pack(side=tk.LEFT)# have to separate the pack() into a new line bc it makes the type NONE
+        tk.Label(heaviside_frame, text="From x = ").pack(side=tk.LEFT)
+        heaviside_start_x = tk.Entry(heaviside_frame, width=5)
+        heaviside_start_x.pack(side=tk.LEFT)
+        tk.Label(heaviside_frame, text="to x = ").pack(side=tk.LEFT)
+        heaviside_end_x = tk.Entry(heaviside_frame, width=5)
+        heaviside_end_x.pack(side=tk.LEFT)
+        heaviside_button = tk.Button(heaviside_frame, text="Add Heaviside", command=lambda:
+                   self.add_function(input_type.LINE, heaviside_amplitude, heaviside_start_x, heaviside_end_x))
+        heaviside_button.pack(side=tk.LEFT, padx=10)
+        return heaviside_frame
+
+    def create_upload_frame(self):
+        # --- Curve Fit File Picker ---
+        upload_frame = tk.Frame(self.select_input_type_frame)
+        upload_frame.pack(side=tk.TOP, fill=tk.X)
+        curve_fit_button = tk.Button(
+            self, text="Select Curve File", command=self.select_curve_file
+        )
+        curve_fit_button.pack()
+
+        self.curve_file_path_var = tk.StringVar(value="")
+        curve_file_label = ttk.Label(self, textvariable=self.curve_file_path_var)
+        curve_file_label.pack()
+
+        return upload_frame
+
+    def show_frame(self, event):
+        selected_frame = self.input_type_options.get()
+        if selected_frame in self.frames:
+            # hide all the frames first
+            for frame in self.frames.values():
+                frame.pack_forget()
+            # but show the selected frame
+            self.frames[selected_frame].pack(fill=tk.BOTH)
+
+        
+    def add_function(self, in_type, arg1, arg2, arg3):
+        if in_type == input_type.LINE:
+            self.custom_functions.append((arg1.get(), arg2.get(), arg3.get()))
+            string_func = "LINE: y = " + arg1.get() + "*x; from x = [" + arg2.get() + " to " + arg3.get() + "]"
+        elif in_type == input_type.HEAVISIDE:
+            self.custom_functions.append((arg1.get(), arg2.get(), arg3.get()))
+            string_func = "HEAVISIDE: amplitude = " + arg1.get() + "; from x = [" + arg2.get() + " to " + arg3.get() + "]"
+        else:
+            return # this function should never be called if the type was anything other than LINE or HEAVISIDE (i.e. it could not be called if type was UPLOAD)
+        self.func_label = ttk.Label(self.see_inputted_functions, text=string_func)
+        self.func_label.pack(side=tk.TOP, pady=5)
 
     def plot_data(self):
         try:
