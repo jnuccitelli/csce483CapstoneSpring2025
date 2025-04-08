12dbc44e167179b9bfe7328cd26f8f1b6ddcabc7
Katelyn Nguyen <heyitsk@tamu.edu>
2025-04-08 01:21:27 -0500

    uh oh almost left in a bug. need to update the curve_fit_settings

--- frontend/optimization_settings/optimization_settings_window.py
+++ frontend/optimization_settings/optimization_settings_window.py
@@ -207,7 +207,7 @@ class OptimizationSettingsWindow(tk.Frame):
         # if self.optimization_type_var.get() == "Maximize/Minimize":
         #     optimization_settings.update(self.max_min_settings.get_settings())
         # elif self.optimization_type_var.get() == "Curve Fit":
-        #     optimization_settings.update(self.curve_fit_settings.get_settings())
+        optimization_settings.update(self.curve_fit_settings.get_settings())
 
         self.controller.update_app_data("optimization_settings", optimization_settings)
 
