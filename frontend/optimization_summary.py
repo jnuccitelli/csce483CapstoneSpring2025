9aabace51f30a47fdd7b28c1c3732f8cb86f395e
jnuccitelli <jnuccitelli1@gmail.com>
2025-04-09 23:21:34 -0500

    threading instead of multiprocessing

--- frontend/optimization_summary.py
+++ frontend/optimization_summary.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import multiprocessing as mp
+import threading as th
 from backend.optimzation_process import optimizeProcess
 
 from matplotlib.figure import Figure
@@ -57,11 +58,11 @@ class OptimizationSummary(tk.Frame):
 
 
         self.queue = mp.Queue()
-        self.process = mp.Process(
+        thread = th.Thread(
             target=optimizeProcess,
             args=(self.queue, curveData, testRows, netlistPath, netlistObject, selectedParameters)
         )
-        self.process.start()
+        thread.start()
 
         self.update_ui()
 
@@ -97,6 +98,4 @@ class OptimizationSummary(tk.Frame):
             self.canvas.draw()
 
     def close_window(self):
-        if self.process.is_alive():
-            self.process.terminate()
         self.parent.quit()
